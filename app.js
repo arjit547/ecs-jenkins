01c67b1c7b1d56f7ea32e481bb628ee419b10b67
arjit547 <97424724+arjit547@users.noreply.github.com>
2023-05-08 12:26:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('DEVOPS-ECS-A2098');
+  res.end('DEVOPS-ECS-A201098');
 });
 
 server.listen(3000, () => {
